9d07183170f33746b3353bfb77837c1a9727b42b
hang-on <asje@me.com>
2016-12-29 23:58:14 +0100

    Add state machine

--- main.asm
+++ main.asm
@@ -1,8 +1,12 @@
 .include "gglib.inc"
 .include "gglib_extended.inc"
 ;
-; Definitions:
-.equ PICO8_PALETTE_SIZE 16
+; Misc. definitions:
+  .equ PICO8_PALETTE_SIZE 16
+; Game states:
+  .equ GS_BOOT 0
+  .equ GS_PREPARE_TITLESCREEN 1
+  .equ GS_RUN_TITLESCREEN 2
 ;
 ;
 ; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -16,14 +20,33 @@
 ; -----------------------------------------------------------------------------
   init:
     ; Run this function once (on game load).
-    ; FIXME: Replace with state machine!
-    ;
-    SELECT_BANK 2
     ; Load the pico-8 palette to colors 16-31.
     ld a,SPRITE_PALETTE_START
     ld b,PICO8_PALETTE_SIZE
     ld hl,pico8_palette
     call load_cram
+    ;
+    ld a,GS_PREPARE_TITLESCREEN
+    ld (game_state),a
+  jp main_loop
+
+  pico8_palette:
+    .dw $0000 $0521 $0527 $0580 $035A $0455 $0CCC $0EFF
+    .dw $040F $00AF $02EF $03E0 $0FA2 $0978 $0A7F $0ACF
+  ; ---------------------------------------------------------------------------
+  main_loop:
+    ; Note: Wait for vblank in the loops!
+    ld a,(game_state)
+    cp GS_PREPARE_TITLESCREEN
+    jp z,prepare_titlescreen
+    cp GS_RUN_TITLESCREEN
+    jp z,run_titlescreen
+    ;
+  jp main_loop
+  ;
+  ; ---------------------------------------------------------------------------
+  prepare_titlescreen:
+    SELECT_BANK 2
     ; Load titlescreen tiles and tilemap to vram.
     ld bc,titlescreen_tiles_end-titlescreen_tiles
     ld de,BACKGROUND_BANK_START
@@ -37,45 +60,25 @@
     ld hl,titlescreen_spritebank_table
     call load_spritebank
     ;
+    ld a,GS_RUN_TITLESCREEN
+    ld (game_state),a
     ; Turn on screen and frame interrupts.
     ld a,DISPLAY_1_FRAME_1_SIZE_0
     ld b,1
     call set_register
-    ; Skip a frame to make sure that we start main at vblank.
     ei
-    call await_frame_interrupt
-  jp main_loop
-  ;
-  pico8_palette:
-    .dw $0000 $0521 $0527 $0580 $035A $0455 $0CCC $0EFF
-    .dw $040F $00AF $02EF $03E0 $0FA2 $0978 $0A7F $0ACF
-  ;
-  titlescreen_spritebank_table:           ; Used by function load_spritebank.
-    .db 0                                 ; Index in spritebank.
-    .dw blinker_tiles_end-blinker_tiles   ; Number of bytes to load.
-    .dw blinker_tiles                     ; Pointer to tile data.
-    .db END_OF_TABLE                      ; Table terminator.
-  ;
-  ; ---------------------------------------------------------------------------
-  main_loop:
-    call await_frame_interrupt
-    call draw
-    ;
-    call update
-    ;
   jp main_loop
   ;
   ; ---------------------------------------------------------------------------
-  draw:
-    ; Draw sprites and background.
-  ret
-  ;
-  ; ---------------------------------------------------------------------------
-  update:
+  run_titlescreen:
+    ; Draw the graphics.
+    call await_frame_interrupt
+    ;
+    ;
     ; Update the game objects.
     ld hl,frame_counter
     inc (hl)
-  ret
+  jp main_loop
   ;
 .ends
 ;
@@ -86,6 +89,12 @@
 ; -----------------------------------------------------------------------------
 .section "title_screen" free
 ; -----------------------------------------------------------------------------
+  titlescreen_spritebank_table:           ; Used by function load_spritebank.
+    .db 0                                 ; Index in spritebank.
+    .dw blinker_tiles_end-blinker_tiles   ; Number of bytes to load.
+    .dw blinker_tiles                     ; Pointer to tile data.
+    .db END_OF_TABLE                      ; Table terminator.
+  ;
   titlescreen_tilemap:
     .include "bank_2\titlescreen_tilemap.inc"
   titlescreen_tiles:
