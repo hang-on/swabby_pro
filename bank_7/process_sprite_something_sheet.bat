d7236ebb02bfccf60dabf77e1dcdb7901255ebe0
hang-on <asje@me.com>
2017-04-09 08:15:12 +0200

    Improve tools

--- bank_7/process_sprite_something_sheet.bat
+++ bank_7/process_sprite_something_sheet.bat
@@ -1,6 +1,10 @@
 echo off
-::
-:: NOTE: Depends on a special pico-8 colormap_cube.png being present in the folder.
+:: process_sprite_something_sheet.bat.
+:: Go to the Dropbox Sprite Something folder and get two spritesheets.
+:: One sheet for 16x16 sprites, and one for 8x8. Merge them with each other,
+:: convert them to pico palette and tiles.
+:: NOTE: Depends on a special pico-8 colormap_cube.png and bmp2tile
+:: being present in the tools folder.
 
 SET folder="C:\Users\ANSJ\Dropbox\Apps\Sprite Something\"
 SET filename1=spritesheet_1.png
@@ -13,21 +17,16 @@ copy %folder%%filename2%
 
 :: Use ImageMagick to insert the 16 pico-8 colors as the first 16 tiles of the
 :: image (in order to preserve the correct palette in the tile conversion.)
-convert colormap_cube.png  %filename1% -append %filename1%
+convert ..\tools\colormap_cube.png  %filename1% -append %filename1%
 
 :: Use ImageMagick to convert the image to 16 colors.
-convert %filename1% -quantize RGB -remap colormap_cube.png  %filename1%
-
-:: Crop away the colormap_cube.
-:: convert %filename1% -crop 32x208+0+32 +repage %filename1%
-:: convert %filename1% -map colormap_cube.png %filename1%
+convert %filename1% -quantize RGB -remap ..\tools\colormap_cube.png  %filename1%
 
 :: Append 8x8 sprites.
 convert -append %filename1% %filename2% %output%
 
-
-:: Use bmp2tile to make tiles out of the appended image.
-bmp2tile.exe %output% -savetiles %output%_tiles.inc -fullpalette -spritepalette -noremovedupes -nomirror -tileoffset 128 -exit
+:: Use bmp2tile (in the tools folder) to make tiles out of the appended image.
+..\tools\bmp2tile.exe %~dp0%output% -savetiles %~dp0%output%_tiles.inc -fullpalette -spritepalette -noremovedupes -nomirror -tileoffset 128 -exit
 
 :: Remove the comments and the first 32 lines (thus removing the tiles of the
 :: color map).
@@ -35,6 +34,8 @@ for /f "skip=32 delims=*" %%a in (%output%_tiles.inc) do (
 echo %%a >>newfile.txt
 )
 xcopy newfile.txt %output%_tiles.inc /y
+
+:: Clean up.
 del newfile.txt /f /q
 del %filename1%
 del %filename2%
